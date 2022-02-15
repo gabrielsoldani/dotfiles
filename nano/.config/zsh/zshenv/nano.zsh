4042e3c431b3b36b73fcc89dc76a04fb82783336
Gabriel Soldani <1268700+gabrielsoldani@users.noreply.github.com>
2022-02-15 13:45:15 -0300

    nano: Fix SpellCheck complaints

--- nano/.config/zsh/zshenv/nano.zsh
+++ nano/.config/zsh/zshenv/nano.zsh
@@ -1,13 +1,15 @@
 # shellcheck shell=sh
 if ! command -v nano >/dev/null 2>&1; then
     echo "nano is not installed" >&2
-    exit 1
+    return 1
 fi
 
 if [ -z "$VISUAL" ]; then
-    export VISUAL="$(command -v nano)"
+    VISUAL="$(command -v nano)"
+    export VISUAL
 fi
 
 if [ -z "$EDITOR" ]; then
-    export EDITOR="$(command -v nano)"
+    EDITOR="$(command -v nano)"
+    export EDITOR
 fi
