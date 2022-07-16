9f9bd6f29ca3fa054eb31b3cbc4a0b84568b6791
Gabriel Soldani <1268700+gabrielsoldani@users.noreply.github.com>
2022-07-16 10:58:07 -0300

    ffmpeg+make: Improve subtitle selection
    Prioritize the languages I usually want.

--- ffmpeg/.config/make/makefile.d/40-ffmpeg-srt.mk
+++ ffmpeg/.config/make/makefile.d/40-ffmpeg-srt.mk
@@ -1,4 +1,7 @@
 .SUFFIXES: .mkv .mp4 .srt
 
 .mkv.srt .mp4.srt:
-	$(FFMPEG) -i $< -map 0:s:a $@
+	# Priority: English > English SDH > Portuguese
+	$(FFMPEG) -i $< -map 0:s:m:language:eng -map -0:s:m:title:SDH $@ \
+		|| $(FFMPEG) -i $< -map 0:s:m:language:eng -map 0:s:m:title:SDH $@ \
+		|| $(FFMPEG) -i $< -map 0:s:m:language:por -map -0:s:m:title:SDH $@
