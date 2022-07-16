3bae766d613a83ab67c0b79f75240a394bc27a91
Gabriel Soldani <1268700+gabrielsoldani@users.noreply.github.com>
2022-07-16 10:53:48 -0300

    ffmpeg+make: Add `FFMPEG` macro
    This lets you override which `ffmpeg` binary is used.

--- ffmpeg/.config/make/makefile.d/40-ffmpeg-copy.mk
+++ ffmpeg/.config/make/makefile.d/40-ffmpeg-copy.mk
@@ -1,4 +1,4 @@
 .SUFFIXES: .mkv .mp4 .mov
 
 .mov.mkv .mp4.mkv .mkv.mp4:
-	ffmpeg -i $< -c copy $@
+	$(FFMPEG) -i $< -c copy $@
--- ffmpeg/.config/make/makefile.d/40-ffmpeg-srt.mk
+++ ffmpeg/.config/make/makefile.d/40-ffmpeg-srt.mk
@@ -1,4 +1,4 @@
 .SUFFIXES: .mkv .mp4 .srt
 
 .mkv.srt .mp4.srt:
-	ffmpeg -i $< -map 0:s:a $@
+	$(FFMPEG) -i $< -map 0:s:a $@
