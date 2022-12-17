2119715472f28f1779d0be138d7ae54eebffd6d5
Gabriel Soldani <1268700+gabrielsoldani@users.noreply.github.com>
2022-12-17 16:08:18 -0300

    tests: Use `mktemp` instead of a fixed IID file name
    This lets us run multiple instances of ./test.sh in parallel :)

--- test.sh
+++ test.sh
@@ -1,22 +1,29 @@
 #!/bin/sh
 # Runs test environments.
-# Usage: test.sh {apt|apk|brew}
+# Usage: ./test.sh {apt|apk|brew}
+
+TEST_SH=$(basename "$0")
+IID_FILE=$(mktemp)
+if [ -z "$IID_FILE" ]; then
+    echo "$TEST_SH: mktemp failed" >&2
+    exit 1
+fi
 
 echo_and_run() {
-    echo "$@"
+    echo "$TEST_SH:" "$@"
     "$@"
 }
 
 cleanup () {
-    echo_and_run docker image rm -f "$(cat test.iid)"
-    echo_and_run rm -f test.iid
+    echo_and_run docker image rm -f "$(cat "$IID_FILE")"
+    echo_and_run rm -f "$IID_FILE"
 }
 
 test() {
     trap exit INT HUP TERM
     trap 'cleanup' EXIT
-    echo_and_run docker build . -f "$1.Dockerfile" --iidfile test.iid
-    echo_and_run docker run --rm -it "$(cat test.iid)"
+    echo_and_run docker build . -f "$1.Dockerfile" --iidfile "$IID_FILE"
+    echo_and_run docker run --rm -it "$(cat "$IID_FILE")"
 }
 
 case "$1" in
