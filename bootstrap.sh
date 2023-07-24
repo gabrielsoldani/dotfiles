3aea63bcf093289d88e1d29ee9ed1a3075ebb570
Gabriel Soldani <gabriel@gabrielsoldani.com>
2023-07-24 18:56:07 -0300

    ci: Test bootstrap script (#5)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
-set -e
 
-has () {
-    command -v "$1" >/dev/null 2>&1
-}
+# Get the absolute path to the directory containing this script.
+DOTFILES_DIR=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)
+
+echo "Running from $DOTFILES_DIR"
+
+# shellcheck source=shell/.config/shell/lib.sh
+if ! . "$DOTFILES_DIR/shell/.config/shell/lib.sh"; then
+    echo "Could not load shell/.config/shell/lib.sh" >&2
+    exit 1
+fi
 
 # Only run sudo if we're not root.
 as_root() {
@@ -32,14 +38,15 @@ install_packages() {
     # Try to initialize Homebrew. This is useful if this script runs
     # just after installing Homebrew, but before Homebrew's dotfiles
     # are stow'd, so running `brew` will fail.
-    . "brew/.config/shell/brew/profile" 2>/dev/null
+    # shellcheck source=brew/.config/shell/profile.d/10-brew.sh
+    . "$DOTFILES_DIR/brew/.config/shell/profile.d/10-brew.sh" 2>/dev/null || true
 
     if has winget.exe; then
         echo "Installing packages using winget..."
         if ! winget.exe import -i winget-packages.jsonc \
              --accept-package-agreements --accept-source-agreements; then
             echo "winget could not install all packages." >&2
-            return 1
+            exit 1
         fi
         echo "Successfully installed packages."
     fi
@@ -48,37 +55,35 @@ install_packages() {
         echo "Installing packages using Homebrew..."
         if ! { brew update && brew bundle --file=Brewfile; } then
             echo "Homebrew could not install all packages." >&2
-            return 1
+            exit 1
         fi
     elif has apt-get; then
         echo "Installing packages using apt-get..."
         if ! { as_root apt-get update \
             && as_root xargs -a apt-packages.txt apt-get install -y; } then
             echo "apt could not install all packages." >&2
-            return 1
+            exit 1
         fi
     elif has apk; then
         echo "Installing packages using apk..."
         if ! { as_root apk update \
             && as_root xargs -a apk-packages.txt apk -v add; } then
             echo "apk could not install all packages." >&2
-            return 1
+            exit 1
         fi
     else
         echo "No package manager found" >&2
-        return 1
+        exit 1
     fi
+
     echo "Successfully installed packages."
+    return 0
 }
 
 stow_shell() {
     ensure_file_does_not_exist "$HOME/.profile"
 
     stow -R shell
-
-    if has git; then
-        git update-index --skip-worktree "shell/.profile"
-    fi
 }
 
 maybe_stow_bash() {
@@ -141,3 +146,4 @@ maybe_stow_zsh
 maybe_stow_git
 
 echo "Bootstrapping complete"
+return 0
