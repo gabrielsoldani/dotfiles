64cf7c2611b8ff3a01a3fb9194142bc126f7ca56
Gabriel Soldani <1268700+gabrielsoldani@users.noreply.github.com>
2023-09-29 14:09:46 -0300

    1password: Improve CLI plugin error message

--- 1password/.config/shell/rc.d/40-1password.sh
+++ 1password/.config/shell/rc.d/40-1password.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=sh
 
-if ! { . "${XDG_CONFIG_HOME:-$HOME/.config}/op/plugins.sh" \
-    || . "$HOME/.op/plugins.sh"; }; then
-    echo "op not installed" >&2
+if ! { source_if_exists "${XDG_CONFIG_HOME:-$HOME/.config}/op/plugins.sh" \
+    || source_if_exists "$HOME/.op/plugins.sh"; }; then
+    echo "op plugins could not be loaded. You may need to run 'op plugin init'." >&2
     return 1
 fi
