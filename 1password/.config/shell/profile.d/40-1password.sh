1f446e16dd9eb0a29bca439b5adb2376f11ff404
Gabriel Soldani <1268700+gabrielsoldani@users.noreply.github.com>
2024-03-11 15:52:10 -0300

    1password: Remove `agent.sock` if it's a broken symlink

--- 1password/.config/shell/profile.d/40-1password.sh
+++ 1password/.config/shell/profile.d/40-1password.sh
@@ -5,10 +5,11 @@ export SSH_AUTH_SOCK="$HOME/.1password/agent.sock"
 
 # For macOS, the agent socket is in an awkward location, so symlink it to the
 # same location as on Linux.
-if [ ! -e "$HOME/.1password/agent.sock" ] \
+if [ ! -e "$SSH_AUTH_SOCK" ] \
     && [ -e "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" ]; then
-    if { mkdir -p "$HOME/.1password" \
-        && ln -s "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" "$HOME/.1password/agent.sock"; } then
+    if { mkdir -p "${SSH_AUTH_SOCK%/*}" \
+        && rm -f "$SSH_AUTH_SOCK" \
+        && ln -s "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" "$SSH_AUTH_SOCK"; } then
         echo "op: symlinked agent socket" >&2
     else
         echo "op: failed to symlink agent socket" >&2
