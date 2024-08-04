99cee80a3bd8eca2dca8efe8153d6ca2cdd150d8
Gabriel Soldani <1268700+gabrielsoldani@users.noreply.github.com>
2024-08-04 20:05:35 -0300

    fzf: Add `fzf`

---
+++ fzf/.config/bash/rc.d/40-fzf.bash
@@ -0,0 +1,4 @@
+# shellcheck shell=bash
+
+# Set up fzf key bindings and fuzzy completion
+eval "$(fzf --bash)"
---
+++ fzf/.config/zsh/rc.d/40-fzf.zsh
@@ -0,0 +1,5 @@
+# shellcheck shell=sh
+
+# Set up fzf key bindings and fuzzy completion
+# shellcheck disable=SC3001,SC3046
+source <(fzf --zsh)
