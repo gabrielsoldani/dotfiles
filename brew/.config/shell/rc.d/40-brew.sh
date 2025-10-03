7950806559c52c093585e4ff1bc8af4e1e1d0e6d
Gabriel Soldani <1268700+gabrielsoldani@users.noreply.github.com>
2025-10-03 12:43:19 -0300

    brew: Update command-not-found initialization

--- brew/.config/shell/rc.d/40-brew.sh
+++ brew/.config/shell/rc.d/40-brew.sh
@@ -1,8 +1,8 @@
 # shellcheck shell=sh
 
-HB_CNF_HANDLER="$(brew --repository)/Library/Taps/homebrew/homebrew-command-not-found/handler.sh"
-if [ -f "$HB_CNF_HANDLER" ]; then
-    . "$HB_CNF_HANDLER"
+HOMEBREW_COMMAND_NOT_FOUND_HANDLER="$(brew --repository)/Library/Homebrew/command-not-found/handler.sh"
+if [ -f "$HOMEBREW_COMMAND_NOT_FOUND_HANDLER" ]; then
+    . "$HOMEBREW_COMMAND_NOT_FOUND_HANDLER"
 else
     echo "homebrew-command-not-found is not installed" >&2
     return 1
