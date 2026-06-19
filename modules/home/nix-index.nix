6183b18c0cf146335a314ac57d366c9476ccbc78
Gabriel Soldani <1268700+gabrielsoldani@users.noreply.github.com>
2026-06-19 00:16:00 -0300

    nix-index-database: Add

---
+++ modules/home/nix-index.nix
@@ -0,0 +1,11 @@
+{ inputs, ... }:
+
+{
+  _class = "homeManager";
+
+  imports = [ inputs.nix-index-database.homeModules.default ];
+
+  programs.nix-index.enable = true;
+
+  programs.nix-index-database.comma.enable = true;
+}
