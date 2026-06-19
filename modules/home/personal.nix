cbcbd72ff4eb01f30a72990bb37af50737059d02
Gabriel Soldani <1268700+gabrielsoldani@users.noreply.github.com>
2026-06-19 00:18:33 -0300

    misc: Use homebrew casks for GUI apps

--- modules/home/personal.nix
+++ modules/home/personal.nix
@@ -1,18 +1,7 @@
-{ lib, pkgs, ... }:
+{ ... }:
 
 {
   _class = "homeManager";
-
-  home.packages =
-    with pkgs;
-    [
-      protonmail-desktop
-      spotify
-    ]
-    ++ lib.optionals stdenv.hostPlatform.isDarwin [
-      daisydisk
-      iina
-    ];
 
   programs.git = {
     settings = {
